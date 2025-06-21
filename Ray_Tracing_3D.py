01d7995c0aff08de014d3319bffa624bd239dfd7
vvalenzuelao <vvalenzuelao@uc.cl>
2025-06-21 01:35:12 -0400

    Add files via upload

--- Ray_Tracing_3D.py
+++ Ray_Tracing_3D.py
@@ -79,9 +79,9 @@ def ElectronEntrancePoint_Camera(xi, yi, zi, Energies, length):
 
 def ElectronExitPoint(Electron_entrances, B, yf, E_z):
     Electron_Exits = []
-    q = 1.602e-19      # Carga del electrón
-    m = 9.109e-31      # Masa del electrón
-    c = 3e8            # Velocidad de la luz
+    q = 1.602e-19      
+    m = 9.109e-31      
+    c = 3e8            
 
     for (x0, y0, z0), E, alpha, beta, unit_vec, unit_vector_3D in Electron_entrances:
         E_J = E * 1.602e-13
@@ -97,7 +97,6 @@ def ElectronExitPoint(Electron_entrances, B, yf, E_z):
             arc_length = r * theta
             t_arc = arc_length / v
 
-            # Componentes de velocidad
             v_perp = v * np.sqrt(unit_vector_3D[0]**2 + unit_vector_3D[1]**2)
             v_par = v * unit_vector_3D[2]
 
@@ -156,7 +155,6 @@ def compute_trajectory(start_point, r, alpha, theta, beta, yff, E, unit_vector_3
     t_vals = np.linspace(0, t_arc, num_points)
     z_arc = z0 + v * unit_vector_3D[2] * t_vals + a_z * t_vals**2 / 2
 
-    # *** FILTRAR TRAJECTORIAS CON z_arc FUERA DE RANGO ***
     if np.any(np.abs(z_arc) >= 0.0025):
         return np.array([]), np.array([]), np.array([])
 
@@ -180,11 +178,11 @@ def compute_trajectory(start_point, r, alpha, theta, beta, yff, E, unit_vector_3
         z_line = np.array([z_arc[-1], zff])
     else:
         delta_y_line = yff - y_arc[-1]
-        t_line = delta_y_line / (v * exit_unit_vector_3D[1])
+        t_line = delta_y_line / (v * tangent_unit[1])
 
-        x_line = np.array([x_arc[-1], x_arc[-1] + v * exit_unit_vector_3D[0] * t_line])
+        x_line = np.array([x_arc[-1], x_arc[-1] + v * tangent_unit[0] * t_line])
         y_line = np.array([y_arc[-1], yff])
-        z_line = np.array([z_arc[-1], z_arc[-1] + v * exit_unit_vector_3D[2] * t_line])
+        z_line = np.array([z_arc[-1], z_arc[-1] + v * tangent_unit[2] * t_line])
         arc_mask = x_arc <= 0.18
         x_arc, y_arc, z_arc = x_arc[arc_mask], y_arc[arc_mask], z_arc[arc_mask]
 
@@ -227,16 +225,13 @@ sm.set_array([])
 cbar = plt.colorbar(sm, ax=ax)
 cbar.set_label('Energía [MeV]')
 
-# Crear planos sólidos en z = 0.0025 y z = -0.0025
 #x_plane = np.linspace(0.025, 0.125, 2)
 #y_plane = np.linspace(0.0, 0.02, 2)
 #x_plane, y_plane = np.meshgrid(x_plane, y_plane)
 
-# Plano superior en z = 0.0025
 #z_plane1 = np.full_like(x_plane, 0.0025)
 #ax.plot_surface(x_plane, y_plane, z_plane1, color='gray')
 
-# Plano inferior en z = -0.0025
 #z_plane2 = np.full_like(x_plane, -0.0025)
 #ax.plot_surface(x_plane, y_plane, z_plane2, color='gray')
 
@@ -244,7 +239,6 @@ ax.zaxis.set_tick_params(pad=12)
 
 plt.tight_layout()
 
-# Gráfico 2D del plano y = yff con puntos (xff, zff)
 fig2, ax2 = plt.subplots(figsize=(8, 6))
 
 xffs = [inter[2][0] for inter in intersections]
@@ -260,10 +254,8 @@ cbar2.set_label('Energía [MeV]')
 
 plt.tight_layout()
 
-# Crear tercer gráfico: xff vs zff para y = 0.05
 fig2, ax2 = plt.subplots(figsize=(10, 6))
 
-# Extraer datos de intersección
 xff_values = []
 zff_values = []
 energies = []
@@ -278,18 +270,14 @@ xff_values = np.array(xff_values)
 zff_values = np.array(zff_values)
 energies = np.array(energies)
 
-# Definir energías de interés
-energies_of_interest = np.arange(0.1, 1.55, 0.1)  # Desde 0.5 a 1.5 MeV
+energies_of_interest = np.arange(0.1, 1.55, 0.1)
 delta_E = 0.05
 
-# Colormap
 cmap = plt.get_cmap('plasma')
 norm = colors.Normalize(vmin=min(energies_of_interest)-0.05, vmax=max(energies_of_interest)+0.05)
 
-# Dibujar puntos de intersección
 sc = ax2.scatter(xff_values, zff_values, c=energies, cmap=cmap, norm=norm, s=5, alpha=0.6)
 
-# Dibujar franjas para energías de interés
 for E_target in energies_of_interest:
     mask = (energies >= E_target - delta_E) & (energies <= E_target + delta_E)
     x_vals = xff_values[mask]
@@ -301,7 +289,6 @@ for E_target in energies_of_interest:
         ax2.axvspan(x_mean - x_std, x_mean + x_std, color=cmap(norm(E_target)), alpha=0.2,
                     label=f'{E_target:.1f} MeV')
 
-# Ajustes del gráfico
 #ax2.set_xlabel('x [m]')
 #ax2.set_ylabel('z [m]')
 #ax2.set_title('Intersección en y = 0.05 m: x vs z')
@@ -311,10 +298,8 @@ for E_target in energies_of_interest:
 #plt.tight_layout()
 #plt.show()
 
-# Crear tercer gráfico: xff vs zff para y = 0.05
 fig2, ax2 = plt.subplots(figsize=(10, 6))
 
-# Extraer datos de intersección
 xff_values = []
 zff_values = []
 energies = []
@@ -329,19 +314,14 @@ xff_values = np.array(xff_values)
 zff_values = np.array(zff_values)
 energies = np.array(energies)
 
-# Definir energías de interés
-energies_of_interest = np.arange(0.1, 1.7, 0.1)  # Desde 0.1 a 1.5 MeV
+energies_of_interest = np.arange(0.1, 1.7, 0.1)
 delta_E = 0.05
 
-# Colormap
 cmap = plt.get_cmap('plasma')
 norm = colors.Normalize(vmin=min(energies_of_interest)-delta_E, vmax=max(energies_of_interest)+delta_E)
 
-# Dibujar puntos de intersección
 sc = ax2.scatter(xff_values, zff_values, c=energies, cmap=cmap, norm=norm, s=5, alpha=0.6)
 
-# Dibujar franjas para energías de interés
-# Dibujar franjas para energías de interés
 for E_target in energies_of_interest:
     mask = (energies >= E_target - delta_E) & (energies <= E_target + delta_E)
     x_vals = xff_values[mask]
@@ -352,23 +332,19 @@ for E_target in energies_of_interest:
         x_mean = np.mean(x_vals)
         franja_ancho = round((x_max - x_min)*100, 1) 
 
-        # Franja de mínimos y máximos
         ax2.axvspan(x_min, x_max, color=cmap(norm(E_target)), alpha=0.2,
                     label=f'{E_target:.1f} MeV')
 
-        # Línea punteada en el promedio
         ax2.axvline(x_mean, color=cmap(norm(E_target)), linestyle='--', linewidth=1)
 
         y_max = ax2.get_ylim()[1]
         ax2.text(x_mean, y_max * 1.01, f'{E_target:.1f} MeV',
                 color=cmap(norm(E_target)), ha='center', va='bottom', rotation=90)
 
-        # Texto con ancho de la franja
-        #y_text = np.min(zff_values) - 0.005  # Un poco debajo de los datos
+        #y_text = np.min(zff_values) - 0.005
         #ax2.text(x_mean, y_text, f'{franja_ancho} cm', ha='top', va='top', fontsize=8)
 
 
-# Ajustes del gráfico
 ax2.set_xlabel('x [m]')
 ax2.set_ylabel('z [m]')
 ax2.set_title('Distribución espacial de electrones sobre la Image Plate', pad=65)
@@ -378,20 +354,14 @@ plt.colorbar(sc, ax=ax2, label='Energía [MeV]', orientation='vertical')
 
 plt.tight_layout()
 
-# Obtener energías de los electrones que llegan a y = 0.05
 energies_at_yff = [E for _, _, _, E, *_ in intersections]
 
-# Crear histograma: número de electrones vs energía
-
-# Centros deseados: 0.1, 0.2, ..., 1.6
 bin_centers = np.arange(0.1, 1.61, 0.1)
-bin_edges = np.arange(0.05, 1.66, 0.1)  # Bordes correspondientes
-# Extraer las posiciones xff de cada electrón que llega a y = 0.05
+bin_edges = np.arange(0.05, 1.66, 0.1)  
+
 xff_values = np.array([intersection[2][0] for intersection in intersections])
 
-# Crear histograma
-bins_edges = np.linspace(0.04, 0.165, 45)  # puedes ajustar el número de bins
-
+bins_edges = np.linspace(0.04, 0.165, 45) 
 plt.figure(figsize=(8, 5))
 plt.hist(energies_at_yff, bins=bin_edges, color='skyblue', edgecolor='black')
 plt.xlabel('Energía [MeV]')
@@ -400,7 +370,6 @@ plt.title('Distribución de energía de electrones sobre la Image Plate')
 plt.grid(True)
 plt.tight_layout()
 
-# Graficar
 #plt.figure(figsize=(10,6))
 #plt.hist(xff_values, bins=bins_edges, color='blue', edgecolor='black')
 #plt.xlabel('Posición en x [m] en y = 0.05 m')
@@ -428,7 +397,6 @@ for i, E in enumerate(sorted_energies):
     x_max = x_vals[-1]
     x_mean = x_vals.mean()
 
-    # Determinar bordes sin solapamiento
     if i > 0:
         prev_x_max = x_vals_dict[sorted_energies[i - 1]][-1]
         left_edge = max(x_min, prev_x_max)
@@ -443,15 +411,13 @@ for i, E in enumerate(sorted_energies):
 
     width = right_edge - left_edge
 
-    # Ahora filtramos los x_vals dentro del rango visual
     valid_vals = x_vals[(x_vals >= left_edge) & (x_vals <= right_edge)]
 
     if width > 0 and len(valid_vals) > 0:
-        centers.append(np.mean(valid_vals))  # x_mean dentro del rango visible
+        centers.append(np.mean(valid_vals))
         widths.append(width)
         heights.append(len(valid_vals))
 
-# Graficar
 #plt.figure(figsize=(10,6))
 #plt.bar(centers, heights, width=widths, align='center', edgecolor='black', color='steelblue')
 #plt.xlabel('Posición en x [m]')
@@ -460,20 +426,13 @@ for i, E in enumerate(sorted_energies):
 #plt.grid(True)
 #plt.tight_layout()
 
-
-# Tu array de posiciones finales en x
 xff_values = np.array([intersection[2][0] for intersection in intersections])
-energies = np.array([intersection[3] for intersection in intersections])  # Energía de cada electrón
+energies = np.array([intersection[3] for intersection in intersections])
 
-# Parámetros
-
-# Crear figura y eje
 fig, ax = plt.subplots(figsize=(10, 6))
 
-# Histograma general
 ax.hist(xff_values, bins=bins_edges, color='blue', edgecolor='black')
 
-# Superponer franjas y líneas por energía
 for E_target in energies_of_interest:
     mask = (energies >= E_target - delta_E) & (energies <= E_target + delta_E)
     x_vals = xff_values[mask]
@@ -483,20 +442,15 @@ for E_target in energies_of_interest:
         x_max = np.max(x_vals)
         x_mean = np.mean(x_vals)
 
-        # Franja entre x_min y x_max
         ax.axvspan(x_min, x_max, color=cmap(norm(E_target)), alpha=0.2,
                    label=f'{E_target:.1f} MeV')
 
-        # Línea punteada en el promedio
         ax.axvline(x_mean, color=cmap(norm(E_target)), linestyle='--', linewidth=1)
 
-        # Texto con energía sobre la línea del promedio
-        # Para ubicarlo, calculamos la altura máxima local
         y_max = ax.get_ylim()[1]
         ax.text(x_mean, y_max * 1.01, f'{E_target:.1f} MeV',
                 color=cmap(norm(E_target)), ha='center', va='bottom', rotation=90)
 
-# Ajustes finales
 ax.set_xlabel('Posición en x sobre la Image Plate [m]')
 ax.set_ylabel('Número de electrones')
 ax.set_title('Distribución espacial de electrones en x sobre la Image Plate', pad=65)
